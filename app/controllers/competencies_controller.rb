7712ee6a4911a545468212e065acc18138f5be11
rahuang <rahuang@andrew.cmu.edu>
2017-03-18 22:26:19 -0400

    Made some temporary changes to support csv upload.

--- app/controllers/competencies_controller.rb
+++ app/controllers/competencies_controller.rb
@@ -1,7 +1,17 @@
 class CompetenciesController < ApplicationController
+  require 'csv'
   # GET /competencies
   # GET /competencies.json
   def index
     @competencies = Competency.all
+  end
+
+  def import
+    file = params[:file]
+    CSV.foreach(file.path, {:headers => true, :encoding => 'ISO-8859-1'}) do |row|
+      puts(file.path)
+      byebug
+    end
+    redirect_to competencies_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :competencies
+  resources :competencies do
+    collection { post :import }
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
